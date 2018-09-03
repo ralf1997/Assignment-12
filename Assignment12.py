8a3a903e503b48ee84c0cade96402afaccce43fd
ralf1997 <rachitgoelchd@gmail.com>
2018-09-03 10:12:48 +0530

    upload

---
+++ Assignment12.py
@@ -0,0 +1,15 @@
+#Q1
+
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+#Q2
+
+import webbrowser
+webbrowser.open("https://www.youtube.com")
+
+#Q3
+import os
+os.rename('file.txt','file.jpg')
